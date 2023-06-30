ad44642422822b51cca104a155832b903cadb99b
Sidjain12 <64673768+Sidjain12@users.noreply.github.com>
2023-06-29 18:33:50 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+var x= "my first console project";
+console.log(x);
+var fname="Sid";
+var lname="Jain";
+var fullname=fname.concat(lname);
+var a="5";
+var b="10";
+var add= a+b;
+var sub=a-b;
+var div=a/b;
+var mult=a*b;
+console.log(add);
+console.log(sub);
+console.log(div)
+console.log(mult);
